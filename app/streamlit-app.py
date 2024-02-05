70e259d2a2525f7cde6ba1ad72f2679cd7b36e22
Ameenah Burhan <ameenah.burhan@gmail.com>
2024-02-05 11:53:25 -0500

    Update streamlit-app.py

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -3,29 +3,50 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="RAG Demo w/ PDF")
     
-    st.header("RAG Demo w/ PDF")
-    st.text_input("Ask me a question about RAG", key="question")
-    
-    st.subheader("Loader Code")
-    loader_code = '''
-        import PyPDF2
-        def load_pdf_data(pdf_file_path):
-            text = ''
-            with open(pdf_file_path, 'rb') as file:
-                pdf_reader = PyPDF2.PdfReader(file)
-                for page in pdf_reader.pages:
-                    text += page.extract_text() + ' '  # Concatenating text from each page
-            return text
-            '''
-    st.code(loader_code, language='python')        
-    st.subheader("Chunking Code")
-    chunking_code = '''
-            '''
-    st.subheader("Chunking Code")
-    embedding_code = '''
-            ''' 
+    st.header("TechSummit 24 Demo")
+    demo_mode = st.checkbox("RAG Demo Mode", value=False)
+
+    st.text_input("Ask me a question", key="question")
+    if demo_mode:    
+        st.subheader("Code", divider='rainbow')
+        st.caption("Loading Code")
+        loader_code = '''
+            import PyPDF2
+            def load_pdf_data(pdf_file_path):
+                text = ''
+                with open(pdf_file_path, 'rb') as file:
+                    pdf_reader = PyPDF2.PdfReader(file)
+                    for page in pdf_reader.pages:
+                        text += page.extract_text() + ' '  # Concatenating text from each page
+                return text
+                '''
+        st.code(loader_code, language='python')        
+        st.caption("Chunking Code")
+        chunking_code = '''
+        text = "..." # your text
+    from langchain.text_splitter import NLTKTextSplitter
+    text_splitter = NLTKTextSplitter()
+    docs = text_splitter.split_text(text)
+
+                '''
+        st.code(chunking_code, language='python')  
+        st.caption("Emedding Code")
+        embedding_code = '''
+        from vertexai.language_models import TextEmbeddingModel
+
+
+    def text_embedding(data) -> list:
+        """Text embedding with a Large Language Model."""
+        model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
+        embeddings = model.get_embeddings(data)
+        for embedding in embeddings:
+            vector = embedding.values
+            print({len(vector)}")
+        return vector
+
+                ''' 
+        st.code(embedding_code, language='python')  
     with st.sidebar:
-        demo_mode = st.checkbox("RAG Demo Mode", value=False)
         if demo_mode: 
             st.subheader("Configurations")
             st.file_uploader("Upload PDF")
